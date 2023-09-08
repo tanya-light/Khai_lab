074f17aa0fc619c127a99d16ba6547a7bb085899
tanya-light <100074292+tanya-light@users.noreply.github.com>
2023-09-08 11:01:22 +0300

    Add files via upload
    319 Sych

---
+++ main.cpp
@@ -0,0 +1,14 @@
+
+#include <iostream> /*library*/
+
+using namespace std; /*prefix std::*/
+
+int main() /*header*/
+{ /*begin*/
+  cout << "hi" << endl; 
+  cout << "fiona";
+  
+    return 0; /*return to os*/
+    
+} 
+/*end*/
