1906760af3c9a5f2b7bb4ae4c6ecae68fc6fe8eb
tanya-light <100074292+tanya-light@users.noreply.github.com>
2023-09-09 15:04:29 +0300

    Add files via upload
    319 sych

---
+++ lab1.cpp
@@ -0,0 +1,13 @@
+
+#include <iostream> //input librarie
+using namespace std; //prefix
+int main() //header
+//beggin
+{ //body
+   
+    int a = 8; //integer a
+    int b = 24; //integer b
+    cout << a+b << endl; //1 operation and new line
+    cout << b/a; //2 operation
+    return 0; //return to os
+} //end
