b7048b83cc82548241d81126b55cff6afe4818e3
tanya-light <100074292+tanya-light@users.noreply.github.com>
2023-09-17 21:03:51 +0300

    Add files via upload
    Sych 319

---
+++ lab_2.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+#include <math.h>
+using namespace std;
+int main()
+{
+   int x,y;
+   cin >> x;
+   y = 3 * x ^ 6 - 6 * x ^ 2 - 7;
+   cout << "y =" << y << endl;
+    return 0;
+}
